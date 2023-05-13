d7267b9f5b4d7b0c72474d2d8e70aaf83e9c9ee1
yoshiyoshiharu <haruki.osaka.u@gmail.com>
2023-05-13 22:21:13 +0900

    fix db inti

--- db/init.go
+++ db/init.go
@@ -4,37 +4,29 @@ import(
 	"database/sql"
 	"github.com/go-sql-driver/mysql"
 	"log"
-	"time"
 )
 
 var Db *sql.DB
 
 func init() {
-	var err
-	jst, err = time.LoadLocation("Asia/Tokyo")
-	if err != nil {
-	  log.Fatal(err)
-	}
-
+	var err error
 	// [TODO] Use envs
-	c := mysql.Config{
+	cfg := mysql.Config{
 		DBName:    "test_db",
 		User:      "root",
 		Passwd:    "root",
 		Addr:      "db:3306",
 		Net:       "tcp",
-		ParseTime: true,
-		Loc:       jst,
 		AllowNativePasswords: true,
 	}
-	Db, err = sql.Open("mysql", c.FormatDSN())
+	Db, err = sql.Open("mysql", cfg.FormatDSN())
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	if err = Db.Ping(); err != nil {
+	err = Db.Ping()
+	if err != nil {
 		log.Fatal(err)
-	} else {
-		log.Println("Successfly connected to database.")
 	}
+	log.Println("Successfly connected to database.")
 }
