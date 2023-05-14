d734dc3abe521667e1b593a25dbd402525a0ffdf
yoshiyoshiharu <haruki.osaka.u@gmail.com>
2023-05-14 20:56:26 +0900

    fix schema.sql

--- db/init/schema.sql
+++ db/init/schema.sql
@@ -1,6 +1,11 @@
 CREATE TABLE IF NOT EXISTS users (
   id INTEGER NOT NULL auto_increment PRIMARY KEY,
   email VARCHAR(255) NOT NULL,
-  frist_name VARCHAR(255) NOT NULL,
+  first_name VARCHAR(255) NOT NULL,
   last_name VARCHAR(255) NOT NULL
-)
+);
+
+INSERT INTO users(email, first_name, last_name) VALUES
+  ("test1@test.com", "Test1", "User1"),
+  ("test2@test.com", "Test2", "User2"),
+  ("test3@test.com", "Test3", "User3");
